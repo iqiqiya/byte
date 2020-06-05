e24b737badc6140c4d8c810be44ccf9a4423ad9b
iqiqiya <43666095+iqiqiya@users.noreply.github.com>
2020-06-05 15:27:36 +0800

    init

---
+++ Python绝技/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author    :iqiqiya
+# @Blog      :77sec.cn
+# @Time      :2020/6/5
+# @FileName  :__init__.py.py
---
+++ 其他/图片批量下载.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author    :iqiqiya
+# @Blog      :77sec.cn
+# @Time      :2020/6/5
+# @FileName  :图片批量下载.py
+import urllib
+
+url = "https://cnstatic01.e.vhall.com/document/539fa66100c9d36a0b811b6b63443c5d/"
+num = 1
+for x in range(500):
+    picUrl = url + str(x) + '.jpg'
+    pic = urllib.urlopen(picUrl)  # 打开图片网址
+    picData = pic.read()  # 读取图片数据
+    picFile = open('pic-' + str(num) + '.jpg', 'wb')  # 打开本地图片文件
+    picFile.write(picData)  # 写入图片数据
+    picFile.close()  # 关闭文件
+    num += 1
